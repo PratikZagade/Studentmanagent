de0ccd37563b26e1d23412a449667ff238711c07
Pratik D. Zagade <pratikzagade7@gmail.com>
2025-07-15 00:49:50 +0530

    My Project on STS

---
+++ StudentManagement/src/main/java/com/example/demo/StudentCOntroller.java
@@ -0,0 +1,71 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentCOntroller {
+	
+	@PostMapping("addStudent")
+	String addStudent(@RequestBody Student s)
+	{
+		//logic
+		
+		return "student details added in db and student name is "  +s.getName();		
+	}
+
+}
+
+class Student
+{
+	private String name;
+	private String address;
+	private String adhar;
+	private String email;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAdhar() {
+		return adhar;
+	}
+	public void setAdhar(String adhar) {
+		this.adhar = adhar;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getRoleNo() {
+		return roleNo;
+	}
+	public void setRoleNo(int roleNo) {
+		this.roleNo = roleNo;
+	}
+	public int getMbNo() {
+		return mbNo;
+	}
+	public void setMbNo(int mbNo) {
+		this.mbNo = mbNo;
+	}
+	private int roleNo;
+	private int mbNo;
+}
